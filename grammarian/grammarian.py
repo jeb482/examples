cecf90236e94a2a0629e6f22e15107dfda1a543e
Jimmy Briggs <jimmy.briggs.256@gmail.com>
2020-12-21 03:58:43 -0500

    Fixed up the grammarian with some comments and a nice argparser

--- grammarian/grammarian.py
+++ grammarian/grammarian.py
@@ -5,7 +5,7 @@ from typing import Dict, Set, Iterable
 import nltk
 from nltk.corpus import words
 
-nltk.download("words")
+
 
 sample_spell_list = \
 [
@@ -25,8 +25,26 @@ sample_spell_list = \
 
 
 def get_single_replacements(spell_name: str, valid_tokens: Set[str]) -> Iterable[str]:
+    """
+    Returns an iterable of every valid English phrase which has an edit distance of exactly
+    one letter from the `spell_name`. Allowable operations are ascii substitutions, not 
+    deletions or insertions. Phrases are considered valid if and only if each token in the
+    phrase is an element of `valid tokens`.
+
+    Args
+    ----
+
+    spell_name: str
+        The name of a spell which can be altered by the Ring of the Grammarian
+
+    valid_tokens: Set[str]
+        A list of every token recognized in the language. 
+    """
     lower_spell_name = spell_name.lower()
-    spell_tokens = [token for token in lower_spell_name.split(" ")] # These tokens should be assumed to exist even if not in the corpus\
+    
+    # We assert that existing tokens in the name of the spell are valid.
+    # If "Tasha's hideous laughter" is a spell, then "Tasha's" is a token.
+    spell_tokens = [token for token in lower_spell_name.split(" ")] 
 
     for character_index in range(len(lower_spell_name)):
         for ascii_index in range(21,126): # punctuation, letters, spaces
@@ -39,7 +57,7 @@ def get_single_replacements(spell_name: str, valid_tokens: Set[str]) -> Iterable
 
 def print_header(text: str):
     """
-    Pretty-print the input text
+    Pretty-print the input `text`
     """
     print("=" * len(text))
     print(text)
@@ -48,7 +66,7 @@ def print_header(text: str):
 
 def print_replacements_for_list(spell_list: Iterable[str], valid_tokens: Set[str]):
     """
-    Pretty-print all the Ring of the Grammarian permutations of the 
+    Pretty-print all the Ring of the Grammarian permutations of the spells in the list.
     """
     for spell_name in spell_list:
         print_header(spell_name)
@@ -57,11 +75,12 @@ def print_replacements_for_list(spell_list: Iterable[str], valid_tokens: Set[str
         print("")
 
 def get_argument_parser():
-    parser = argparse.ArgumentParser(description="Find all the permutations of spells allowed by the Ring of the Grammarian")
-    parser.add_argument('spells', metavar='S', type=str, nargs='+', help='the names of spells to permute')
+    parser = argparse.ArgumentParser(description="Find all the permutations of spells allowed by the Ring of the Grammarian.")
+    parser.add_argument('spells', metavar='"<Spell Name>"', type=str, nargs='+',
+                         help='the names of spells to permute (put multi-word names in quotes)')
     return parser
 
 if __name__ == "__main__":
     spells = get_argument_parser().parse_args().spells
-    print(spells)
-    print_replacements_for_list(sample_spell_list, set(words.words()))
+    nltk.download("words")
+    print_replacements_for_list(spells, set(words.words()))
